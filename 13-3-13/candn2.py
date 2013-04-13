b8df6dc43491df078241d33317551ceffef0d3db
Praveen Venkatesh <praveenv253@gmail.com>
2013-04-13 20:26:19 +0530

    Add solutions for CANDN. - First two use a theoretically faster but practically slower approach, so   they TLE'd. - The last one uses a "dumb" approach, but hey, it works!

---
+++ 13-3-13/candn2.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+
+import psyco
+psyco.full()
+
+# A graph shall be represented in adjacency list format:
+# The set of vertices is a list, and each element of the list links to a list
+# of tuples (other_node, weight, pointer to Q element)
+# Apart from this, we also need to maintain a heap, in order to be able to
+# quickly find the minimum current distance estimate
+def main():    
+    import sys
+    import heapq
+    
+    while(1):
+        # Number of vertices and edges
+        v_, b, c, n, k = sys.stdin.readline().split()
+        v_, b, c, n, k = int(v_), int(b), int(c), int(n), int(k)
+        if [v_, b, c, n, k] == [-1, -1, -1, -1, -1]:
+            break
+        
+        b -= 1
+        c -= 1
+        n -= 1
+        
+        Q = []                          # Initialize empty heap
+        V = []                          # Initialize empty vertex list
+        Qdict = []                      # Used to get the ith node's info
+        Vdict = []
+        for i in xrange(v_):
+            Qelem = [float('inf'), i]   # Heap for extractmin operation
+            heapq.heappush(Q, Qelem)
+            V.append(([], []))          # Initialize empty adjacency lists
+            Qdict.append(Qelem)         # Create pointers to ith node
+            Vdict.append(V[i])
+        for edge_index in xrange(k):
+            # Assume no multiple edges in input
+            i, j, w = sys.stdin.readline().split()
+            i, j, w = int(i), int(j), int(w)
+            i -= 1
+            j -= 1
+            # Undirected graph => add both directions
+            V[i][0].append((j, w))
+            V[j][0].append((i, w))
+        
+        Qdict[b][0] = 0
+        
+        # Here starts Dijkstra (starting from bar)
+        while Q:
+            
+            # u and v are elements of Q
+            u = heapq.heappop(Q)
+            node_index = u[1]
+            if node_index < 0:                  # Check if the node index is a
+                                                # valid one
+                continue
+            
+            # Iterate through adjacent nodes
+            for adj_node in Vdict[node_index][0]:
+                adj_node_index = adj_node[0]
+                v = Qdict[adj_node_index]       # Adjacent node's dist estimate
+                if v[0] > u[0] + adj_node[1]:   # if d(src, v) > d(src, u) + w
+                    v_new = v[:]                # Create a copy of the Q elem
+                                                # to modify
+                    v_new[0] = u[0] + adj_node[1]
+                    v[1] = -1                   # Remove the previous Q elem by
+                                                # setting its node index to -1
+                    Qdict[adj_node_index] = v_new
+                    heapq.heappush(Q, v_new)    # Add the new Qelem
+                    del Vdict[adj_node_index][1][:]
+                    Vdict[adj_node_index][1].append((node_index, adj_node[1]))
+                                                # Construct parent tree
+                elif v[0] == u[0] + adj_node[1]:
+                    Vdict[adj_node_index][1].append((node_index, adj_node[1]))
+        
+        # Done with Dijkstra from bar!
+        
+        c_min_dist = Qdict[c][0]
+        n_min_dist = Qdict[n][0]
+        
+        # Now to do Dijkstra starting from Charly's house
+        
+        Q = []                          # Re-initialize empty heap
+        Qdict = []                      # Used to get the ith node's info
+        for i in xrange(v_):
+            Qelem = [float('inf'), i]   # Heap for extractmin operation
+            heapq.heappush(Q, Qelem)
+            Qdict.append(Qelem)         # Create pointers to ith node
+        
+        Qdict[c][0] = 0
+        
+        while Q:
+            
+            # u and v are elements of Q
+            u = heapq.heappop(Q)
+            node_index = u[1]
+            if node_index < 0:                  # Check if the node index is a
+                                                # valid one
+                continue
+            
+            # Iterate through adjacent nodes in the parent tree
+            for adj_node in Vdict[node_index][1]:
+                adj_node_index = adj_node[0]
+                v = Qdict[adj_node_index]       # Adjacent node's dist estimate
+                if v[0] > u[0] + adj_node[1]:   # if d(src, v) > d(src, u) + w
+                    v_new = v[:]                # Create a copy of the Q elem
+                                                # to modify
+                    v_new[0] = u[0] + adj_node[1]
+                    v[1] = -1                   # Remove the previous Q elem by
+                                                # setting its node index to -1
+                    Qdict[adj_node_index] = v_new
+                    heapq.heappush(Q, v_new)    # Add the new Qelem
+        
+        def find_split_point(node_index, split_point):
+            for adj_node in Vdict[node_index][1]:
+                adj_node_index = adj_node[0]
+                v = Qdict[adj_node_index]
+                if v[0] < split_point[0]:
+                    split_point[0] = v[0]
+                    split_point[1] = v[1]
+                find_split_point(adj_node_index, split_point)
+        
+        node_index = n
+        split_point = [float('inf'), -1]
+        find_split_point(node_index, split_point)
+        
+        # This is the distance to the split point from Charly's house
+        split_point_dist = split_point[0]
+        print (c_min_dist - split_point_dist), 
+        print split_point_dist, (n_min_dist + split_point_dist - c_min_dist)
+
+
+if __name__ == '__main__':
+    main()
